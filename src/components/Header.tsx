d027457d9e3d48ff377ff5ef582029b319e1f7e3
Luismkm <luismarcelo13@yahoo.com.br>
2022-08-07 20:06:39 -0300

    feat: fix header on scroll

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import { Logo } from "./Logo";
 
 export function Header() {
   return (
-    <header className="w-full py-5 flex items-center justify-center bg-gray-700 border-b border-gray-600">
+    <header className="w-full py-5 flex items-center justify-center bg-gray-700 border-b border-gray-600 fixed top-0 left-0 z-10">
       <Logo />
     </header>
   )
