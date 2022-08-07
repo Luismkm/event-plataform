b90c51b14315708c400546326b54d8a3816e637d
Luismkm <luismarcelo13@yahoo.com.br>
2022-08-07 20:00:37 -0300

    refactor: remove console log

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,10 +4,8 @@ import { Lesson } from "./Lesson";
 export function Sidebar() {
   const { data } = useGetLessonsQuery()
 
-  console.log(data)
-
   return (
-    <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
+    <aside className="w-[348px] h-screen bg-gray-700 p-6 border-l border-gray-600 fixed top-19 right-0">
       <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
         Cronograma de aulas
       </span>
