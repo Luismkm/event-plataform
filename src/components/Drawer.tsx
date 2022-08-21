bd655aa483fcd671acd896ec9c25e6c0fce4000b
Luismkm <luismarcelo13@yahoo.com.br>
2022-08-21 14:55:08 -0300

    feat: get data from CDN to Lesson components

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -1,8 +1,10 @@
 import { useSidebarDrawer } from "../context/SidebarDrawerContext";
+import { useGetLessonsQuery } from "../graphql/generated";
 import { Lesson } from "./Lesson";
 
 export function Drawer() {
   const { setIsMenuLessonsActive } = useSidebarDrawer()
+  const { data } = useGetLessonsQuery()
 
   return (
     <>
@@ -15,44 +17,44 @@ export function Drawer() {
             className="drawer-overlay" 
             onClick={() => setIsMenuLessonsActive(false)}>
             </label>
-          <aside className="menu p-4 overflow-y-auto bg-gray-700 text-base-content h-[100%] max-h-screen border-l border-gray-600">
+          <aside className="menu p-4 overflow-y-auto bg-gray-700 text-base-content h-[100%] w-[360px] xl:w-[348px] max-h-screen border-l border-gray-600">
             <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
               Cronograma de aulas
             </span>
-            <div className="flex flex-col gap-8 w-[320px] px-6">
-              <Lesson
-                key=""
-                title="Abertura do evento Ignite labs"
-                slug=""
-                availabletAt={new Date()}
-                type="live"
-              />
+            <div className="flex flex-col gap-8 w-[300px] mx-auto">
+            {data?.lessons.map(lesson => {
+              return (
+                <Lesson
+                  key={lesson.id}
+                  title={lesson.title}
+                  slug={lesson.slug}
+                  availabletAt={new Date(lesson.availableAt)}
+                  type={lesson.lessonType}
+                />
+              )
+            })}
             </div>
           </aside>
         </div>
       </div>
 
       {/*  SIDERBAR  1280 > PX */}
-      <aside className="menu p-4 overflow-y-auto bg-gray-700 text-base-content h-[100%] max-h-screen border-l border-gray-600 hidden xl:block">
+      <aside className="menu p-4 overflow-y-auto bg-gray-700 text-base-content min-h-[1223px] border-l border-gray-600 hidden xl:block">
           <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
             Cronograma de aulas
           </span>
           <div className="flex flex-col gap-8 w-[320px] px-2">
-            <Lesson
-              key=""
-              title="Abertura do evento Ignite labs"
-              slug=""
-              availabletAt={new Date()}
-              type="live"
-            />
-
-            <Lesson
-              key=""
-              title="Abertura do evento Ignite labs"
-              slug=""
-              availabletAt={new Date()}
-              type="live"
-            />
+          {data?.lessons.map(lesson => {
+            return (
+              <Lesson
+                key={lesson.id}
+                title={lesson.title}
+                slug={lesson.slug}
+                availabletAt={new Date(lesson.availableAt)}
+                type={lesson.lessonType}
+              />
+            )
+        })}
           </div>
         </aside>
     </>
